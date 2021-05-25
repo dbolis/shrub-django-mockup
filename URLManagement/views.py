f34825f4ad17bbf7f1868c0fa30cf0e7a54cecf5
Daniel Bolis <daniel@Daniels-MacBook-Pro-2.local>
2021-05-25 01:36:25 -0700

    started django project

---
+++ URLManagement/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def landing(request):
+    return render(request,'landing.html')
+
+def values(request):
+    return render(request,'values.html')
