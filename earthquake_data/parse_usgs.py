b8934091605dad0fa0b2d6fe9cbfeaceec5f5f30
Michael Vander Meiden <michaelvandermeiden@gmail.com>
2021-06-01 15:26:05 -0400

    merged master and made some edits

--- earthquake_data/parse_usgs.py
+++ earthquake_data/parse_usgs.py
@@ -1,6 +1,22 @@
 import requests
 import json
 import pdb
+import pymysql
+
+
+
+#set up DB stuff
+
+endpoint = 'shrub-earthquake.cluster-cq8uib2zuvo7.us-west-1.rds.amazonaws.com'
+username = 'admin'
+password = 'admin'
+database_name = 'shrub_earthquake'
+
+
+
+connection = pymysql.connect(host=endpoint, user=username, passwd=password, db=database_name)
+
+
 
 response = requests.get("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_hour.geojson")
 
@@ -8,7 +24,15 @@ geo_json = json.loads(response.content)
 
 earthquakes = geo_json['features']
 
+
+
+
 for item in earthquakes:
+
+    #check if item exists in table
+
+
+    #parse relevant properties
     print("~~~~~~~~~~~~~~~~~~~~~~~")
     id = item["id"]
     print(id)
